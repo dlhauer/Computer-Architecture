ace01ff8bbfa310735857134bd8c356497ffc455
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-05-18 13:10:56 -0700

    adds ram_read and ram_write methods

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -26,6 +26,11 @@ class CPU:
             self.ram[address] = instruction
             address += 1
 
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+        
+    def ram_write(self, MDR, MAR):
+        self.ram[MAR] = MDR
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
