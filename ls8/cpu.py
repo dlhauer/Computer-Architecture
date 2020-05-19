ca04353c3106df225ec20065c4b2f6e7df5fb774
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-05-19 08:57:55 -0700

    adds MUL instruction handler

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -11,6 +11,7 @@ class CPU:
             'LDI': 0b10000010,
             'PRN': 0b01000111,
             'HLT': 0b00000001,
+            'MUL': 0b10100010
         }
 
     def load(self):
@@ -35,7 +36,8 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -79,6 +81,8 @@ class CPU:
                 self.reg[operand_a] = operand_b
             elif IR == self.instructions['PRN']:
                 print(self.reg[operand_a])
+            elif IR == self.instructions['MUL']:
+                self.alu('MUL', operand_a, operand_b)
             elif IR == self.instructions['HLT']:
                 halted = True
             
