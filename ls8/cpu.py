5bac20c8385b4c8d607a46dd946606fee280ff6d
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-05-19 08:50:42 -0700

    adds file reading

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -15,16 +15,10 @@ class CPU:
 
     def load(self):
         address = 0
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        program = []
+        f = open(f'ls8/{sys.argv[1]}', 'r')
+        program = f.read().split('\n')
+        program = [int(line.split('#')[0], 2) for line in program if line != '' and line[0] != '#']
 
         for instruction in program:
             self.ram[address] = instruction
